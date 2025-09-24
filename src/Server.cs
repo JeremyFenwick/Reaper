f2e5ca3493b06198642144f5da8682cd6ff64c99
Jeremy Fenwick <jeremy.fenwick@gmail.com>
2025-09-24 18:44:04 +1000

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -11,4 +11,13 @@ public class Server(int port)
         _listener.Start();
         var client = _listener.AcceptTcpClient();
     }
+
+    private async Task HandleClient(TcpClient client)
+    {
+        await using var stream = client.GetStream();
+        await using var writer = new StreamWriter(stream);
+        var buffer = new byte[1024];
+        var bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+        await writer.WriteAsync("+PONG\\r\\n");
+    }
 }
