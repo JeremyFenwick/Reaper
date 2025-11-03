d26999b5ab049b06a883023d7be7b3eb672ea1ba
Madvillain <jeremy.fenwick@gmail.com>
2025-11-03 21:51:04 +1100

    codecrafters submit [skip ci]

--- src/redis_data_store/KeyValueStore.cs
+++ src/redis_data_store/KeyValueStore.cs
@@ -90,8 +90,8 @@ public class KeyValueStore
 
         List<string> SafeSlice(List<string> list, int start, int end)
         {
-            if (start < 0) start = list.Count - start;
-            if (end < 0) end = list.Count - end;
+            if (start < 0) start = list.Count + start;
+            if (end < 0) end = list.Count + end;
             return list.Skip(start).Take(end - start).ToList();
         }
     }
